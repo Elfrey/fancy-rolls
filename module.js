6eea1bf4c8ac5a358adf76ac3b4b599b26616ca2
Elfery <elfrey@mail.ru>
2023-09-20 09:48:50 +0500

    v0.2

--- module.js
+++ module.js
@@ -1,14 +1,17 @@
-const formatMessages = ["ability", "save", "skill"];
+const formatMessages = ["ability", "save", "skill", "check"];
 Hooks.on("renderChatMessage", (message, $html) => {
   const speaker = message.speaker.actor;
   const actor = game.actors.get(speaker);
-  const $img = $(`<img src="${actor.img}" alt="${actor.name}" width="36" height="36" />`);
+  const tokenImg = actor.prototypeToken.texture.src;
+  const $img = $(`<img src="${tokenImg}" alt="${actor.name}" title="${actor.name}" width="36" height="36" />`);
   const $flavorText = $html.find(".flavor-text");
+  console.debug("formatMessages", formatMessages);
+  console.log("(message.flags.dnd5e.roll", message.flags.dnd5e.roll);
   if (formatMessages.includes(message.flags.dnd5e.roll.type)) {
     const replaceText = game.i18n.localize("cr.replaceText");
     $flavorText.addClass("flavor-text-fr");
     if ($flavorText.text().includes(replaceText)) {
-      $flavorText.text((index, currentText) => {
+      $flavorText.text((_, currentText) => {
         return currentText.replace(`${replaceText} `, "");
       });
     }
@@ -28,11 +31,12 @@ Hooks.on("renderChatMessage", (message, $html) => {
           break;
       }
       if (advantageModeIcon !== "") {
-        $flavorText.html((index, currentText) => {
+        const currentModText = roll.options.advantageMode === 1 ? game.i18n.localize("cr.advantage") : game.i18n.localize("cr.disadvantage");
+        $flavorText.html((_, currentText) => {
           return currentText.replace(` (${game.i18n.localize("cr.advantage")})`, "").replace(` (${game.i18n.localize("cr.disadvantage")})`, "");
         });
         $html.find(".dice-formula").append(
-          `<img src="icons/svg/${advantageModeIcon}.svg" class="fr-advantage-icon fr-advantage-icon--${advantageModeIcon}" width="24" height="24" />`
+          `<img src="icons/svg/${advantageModeIcon}.svg" class="fr-advantage-icon fr-advantage-icon--${advantageModeIcon}" title="${currentModText}" alt="${currentModText}" width="24" height="24" />`
         );
       }
     }
